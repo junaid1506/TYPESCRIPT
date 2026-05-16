7f5d5701ac9585715c4659edaa7de3251196c50b
junaid1506 <junnu2757@gmail.com>
2026-05-16 18:19:57 +0530

    Update

--- index.ts
+++ index.ts
@@ -1,1 +1,29 @@
-var a = 1
+// // // premitive types
+// // var a = 12
+// // var b = "hello"
+// // var c = true
+
+// // // reference types
+// // var d = [1, 2, 3]
+// // var e = { name: "Alice", age: 30 }
+// // var f = function (x: number) {
+// //     return x * 2
+// // }
+
+// var a = 12
+// var b = a
+
+// console.log(a) // 12
+// a = 20
+// console.log(a) // 20
+// console.log(b) // 12
+
+// var c = [1, 2, 3]
+// var d = c
+
+// console.log(c) // [1, 2, 3]
+// c.push(4)
+// console.log(c) // [1, 2, 3, 4]
+// console.log(d) // [1, 2, 3, 4]
+
+

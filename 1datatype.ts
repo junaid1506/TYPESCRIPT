8ad3e75cf48ef2392df163f215955bfc8b14e588
junaid1506 <junnu2757@gmail.com>
2026-05-18 18:21:27 +0530

    Update

--- 1datatype.ts
+++ 1datatype.ts
@@ -1,2 +1,9 @@
 let name : string =  "junaid"
-name =  
+let arr : number[] = [1,2,3,4,5]
+let arr2 : Array<string> = ["a","b","c","d"]
+let tuple : [string,number] = ["junaid", 23]
+enum Color {Red, Green, Blue}
+enum userRole {admin = "admin", user = "user", guest = "guest"}
+enum statusCode {success = 200, notFound = 404, serverError = 500}
+
+

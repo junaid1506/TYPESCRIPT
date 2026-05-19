1ecf1866cc6cf5d6737024f682b385db7369ab15
junaid1506 <junnu2757@gmail.com>
2026-05-19 18:09:09 +0530

    Update

--- 1datatype.ts
+++ 1datatype.ts
@@ -1,9 +1,26 @@
-let name : string =  "junaid"
-let arr : number[] = [1,2,3,4,5]
-let arr2 : Array<string> = ["a","b","c","d"]
-let tuple : [string,number] = ["junaid", 23]
-enum Color {Red, Green, Blue}
-enum userRole {admin = "admin", user = "user", guest = "guest"}
-enum statusCode {success = 200, notFound = 404, serverError = 500}
+// let name : string =  "junaid"
+// let arr : number[] = [1,2,3,4,5]
+// let arr2 : Array<string> = ["a","b","c","d"]
+// let tuple : [string,number] = ["junaid", 23]
+// enum Color {Red, Green, Blue}
+// enum userRole {admin = "admin", user = "user", guest = "guest"}
+// enum statusCode {success = 200, notFound = 404, serverError = 500}
+
+// let a; // tyepe any
+// let b: unknown; // type unknown
+// let c: never; // type never
+
+// b = "hello"
+// b = 123
+// b = true
+
+// function abc(): void {
+//   console.log("This function never returns")
+// }
+
+// function def(): never {
+//   throw new Error("This function always throws an error")
+// }
+
 
 
